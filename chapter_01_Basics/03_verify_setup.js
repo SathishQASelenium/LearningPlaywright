84c176975af032931b6b1d4359d38c7e1ee2cb0e
SathishQASelenium <selenium_test@hotmail.com>
2026-02-12 08:31:21 +0530

    Initial commit from assistant

---
+++ chapter_01_Basics/03_verify_setup.js
@@ -0,0 +1,10 @@
+console.log(process.platform);
+// MAC - DARWIN
+// WINDOWS - WIN32
+// LINUX - LINUX
+
+console.log(process.arch);
+// 64 - x64
+// 32 - x86
+
+console.log("Node Version: " + process.version);
