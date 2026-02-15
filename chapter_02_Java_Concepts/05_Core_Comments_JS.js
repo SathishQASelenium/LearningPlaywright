680381a62043c61bad5747fa68691fb08ee7cfd4
SathishQASelenium <selenium_test@hotmail.com>
2026-02-15 13:41:09 +0530

    chore: add new chapter_02 files

---
+++ chapter_02_Java_Concepts/05_Core_Comments_JS.js
@@ -0,0 +1,18 @@
+// This is comment in JavaScript - This code will not be executed
+console.log("Hello, World!");
+
+/**
+ * This is a multi-line comment in JavaScript.
+ * Author: Sathish Kumar A S
+ * Date: 2024-06-01
+ * Version: 1.0
+ * Description: This code demonstrates the use of comments in JavaScript.
+ **/
+
+/*
+ * This is a multi-line comment in JavaScript.
+ * Author: Sathish Kumar A S
+ * Date: 2024-06-01
+ * Version: 1.1
+ * Description: This code demonstrates the use of comments in JavaScript.
+ */
